b66cf113de0eddec2e0ca7330ad64609100b4ffd
unknown <Halila2002>
2024-12-27 14:30:53 +0100

    modif

--- index.php
+++ index.php
@@ -56,8 +56,8 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="navbar-nav ms-auto py-0">
                         <a href="index.php" class="nav-item nav-link active">Acceuil</a>
-                        <a href="index.php" class="nav-item nav-link">A propos</a>
-                        <a href="about.php" class="nav-item nav-link">S'informer</a>
+                        <a href="about.php" class="nav-item nav-link">A propos</a>
+                        <a href="informe.php" class="nav-item nav-link">S'informer</a>
                         <a href="hopital.php" class="nav-item nav-link">Se soigner</a>
                         <a href="formation.php" class="nav-item nav-link">Se former</a>
                         </div>
--- informe.php
+++ informe.php
@@ -57,7 +57,7 @@ $stmt = $connexion->query ('SELECT * FROM actualite');
                     <div class="navbar-nav ms-auto py-0">
                         <a href="index.php" class="nav-item nav-link">Acceuil</a>
                         <a href="about.php" class="nav-item nav-link">A propos</a>
-                        <a href="about.php" class="nav-item nav-link active">S'informer</a>
+                        <a href="informe.php" class="nav-item nav-link active">S'informer</a>
                         <a href="hopital.php" class="nav-item nav-link">Se soigner</a>
                         <a href="formation.php" class="nav-item nav-link">Se former</a>
 

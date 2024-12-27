fc207a6ab95573665780a9bfe3f9706377f6a72c
unknown <Halila2002>
2024-12-27 12:22:04 +0100

    Ajout des modifications

--- bdd.php
+++ bdd.php
@@ -5,6 +5,7 @@ $motDePasse = "3hSJAVicQCMEnhh";
 $baseDeDonnees ="u924654367_dokotoro";
 
 try {
+    
     $connexion = new PDO("mysql:host=$serveur;dbname=$baseDeDonnees", $utilisateur, $motDePasse);
 } catch(PDOException $e) {
     echo "Échec de la connexion : " . $e->getMessage();
